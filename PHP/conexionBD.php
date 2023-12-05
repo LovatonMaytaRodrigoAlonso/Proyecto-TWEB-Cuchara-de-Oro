d3987c8afc3feb1f390b159e83012638320e2d19
Rodrigo <rlovatonm@gmail.com>
2023-12-05 16:30:11 -0500

    Pequeños cambios innecesarios

--- PHP/conexionBD.php
+++ PHP/conexionBD.php
@@ -5,7 +5,7 @@
     $username = "root";
     $password = "";
     $database = "cuchara_de_oroo";
-    $port = "3306";
+    $port = "3307";
   
     $con = mysqli_connect($hostname, $username, $password, $database, $port) or die("Error al conectar a la Base de Datos");
     return $con;
