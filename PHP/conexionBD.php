a04ba5122f536f78fbb5e98b2ef39535c4d30a1e
ReyesSalazarJohn <>
2023-12-05 16:24:08 -0500

    cambios_pago

--- PHP/conexionBD.php
+++ PHP/conexionBD.php
@@ -4,8 +4,8 @@
     $hostname = "localhost";
     $username = "root";
     $password = "";
-    $database = "cuchara_de:_oro";
-    $port = "3307";
+    $database = "cuchara_de_oroo";
+    $port = "3306";
   
     $con = mysqli_connect($hostname, $username, $password, $database, $port) or die("Error al conectar a la Base de Datos");
     return $con;
