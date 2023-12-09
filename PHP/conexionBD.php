5e3c0cc08ac0043a24d83f6571f590c93ff6fbaf
misaj <misaj@DESKTOP-IO04IL5>
2023-12-09 12:44:17 -0500

    Cambios_Sebastian: Solucion de error en el registro, por un innombrable que lo cambio >:c

--- PHP/conexionBD.php
+++ PHP/conexionBD.php
@@ -4,10 +4,12 @@
     $hostname = "localhost";
     $username = "root";
     $password = "";
-    $database = "cuchara_de_oroo";  
+    $database = "cuchara_de_oro";  
     $port = "3306";
   
+    global $con;
     $con = mysqli_connect($hostname, $username, $password, $database, $port) or die("Error al conectar a la Base de Datos");
+
     return $con;
 }
 
