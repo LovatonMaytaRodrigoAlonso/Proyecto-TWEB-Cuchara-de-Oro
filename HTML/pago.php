66676b8ffe1b95496652b5dbe5c7e103b4ec26b0
ReyesSalazarJohn <>
2023-12-12 13:48:39 -0500

    cambios_tarjeta

--- HTML/pago.php
+++ HTML/pago.php
@@ -7,10 +7,7 @@
     <title>Carrito de compra | Cuchara de Oro</title>
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="icon" type="image/x-icon" href="../Imagenes/Cuchara de Oro circulo.png">
-    <link rel="stylesheet" href="../CSS/reclamaciones.css">
-
-    <link rel="stylesheet" href="../CSS/footer1.css">
-    <link rel="stylesheet" href="../CSS/home.css">
+    <link rel="stylesheet" href="../CSS/pago.css">
 </head>
 
 <body>
@@ -18,9 +15,10 @@
         <h1>Carrito de compras</h1>
         <h2>Cuchara de Oro</h2>
     </header>
-        <main id="mainContainer" > </main>
+        <main id="mainContainer"> </main>
 
         <main>
+          <div id="datos">  
             <h2>Datos Personales</h2>
 
             <form method="post" action="../PHP/ProcesoPedidos.php">
@@ -32,18 +30,24 @@
                 <input type="text" id="fono" name="fono" required>
 
                 <h2>Método de Pago</h2>
-                <input type="radio" id="yape" name="metodoPago" value="yape" required>
-                <label for="yape">Yape</label>
+                  <div id="metodoPagoContainer">
+                  <input type="radio" id="yape" name="metodoPago" value="yape" required>
+                  <label for="yape">Yape</label>
 
-                <input type="radio" id="tarjeta" name="metodoPago" value="tarjeta" required>
-                <label for="tarjeta">Tarjeta de Crédito</label>
+                 <input type="radio" id="tarjeta" name="metodoPago" value="tarjeta" required>
+                   <label for="tarjeta">Tarjeta de Crédito</label>
+                   </div>
 
-                
+               <input type="hidden" name="accion" value="add">
+                <center>
+                  <button class="btn_terminar" type="button" onclick="procesarPago()">Procesar pago</button>
+                </center>
             </form>
+           </div>
 
-             <center>
-                <button class="btn_terminar" type="submit" onclick="procesarPago()">Procesar pago</button>
-            </center> 
+            <!-- <center>
+                button type="submit" value="PagarPedido">Pagar</button>
+            </center> -->
         </main>
     <script>
         function procesarPago() {
